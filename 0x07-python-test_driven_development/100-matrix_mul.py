4e6803f0b907c6556040a884fd47de144dcadfee
Olaniyi2959 <olaniyiafolayan@yahoo.com>
2024-01-08 14:55:06 -0800

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 100-matrix_mul.py
-# Victoria Oluwabunmi Olabode <bhummhie97@gmail.com>
+# Afolayan Olaniyi Joseph <olaniyiafolayan@yahoo.com>
 """Defines a matrix multiplication function."""
 
 
