6ca4a023491999c216342d1ee9727fcc59bafc31
Olaniyi2959 <olaniyiafolayan@yahoo.com>
2024-03-28 04:44:42 -0700

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,43 @@
-#1/usr/bin/python3
-"""You must use the GitHub API, here is the documentation https:
-    //developer.github.com/v3/repos/commits/
+#!/usr/bin/python3
 """
-
+Module to access to the GitHub API and uses the information
+"""
 import requests
-import sys
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+def main(argv):
+    """
+    Function that list 10 commits (from the most recent to oldest)
+    of the repository.The first argument will be the repository name
+    and the second argument will be the owner name
+    """
+
+    def print_commits(i, commit_list):
+        """
+        List the commits, less than 10 commits
+        """
+        sha = commit_list[i].get('sha')
+        commit = commit_list[i].get('commit')
+        author = commit.get('author')
+        name = author.get('name')
+        print('{}: {}'.format(sha, name))
+
+    repo = argv[1]
+    owner = argv[2]
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    response = requests.get('https://api.github.com/repos/' + owner +
+                            '/' + repo + '/commits', headers=headers)
+    commit_list = response.json()
+    size = len(commit_list)
+    if size < 10:
+        for i in range(0, size):
+            print_commits(i, commit_list)
+    else:
+        for i in range(0, 10):
+            print_commits(i, commit_list)
+
 
 if __name__ == "__main__":
-
-    repository_name = sys.argv[1]
-    owner_name = sys.argv[2]
-    url = https://api.github.com/repos/{}/{}/commits"\
-        .format(owner_name, repository_name)
-    data = requests.get(url)
-    for idx, value in enumerate(data.json()):
-        if idx == 10:
-            break
-        print("{}: {}"
-              .format(value.get('sha'),
-                      value['commit']['author']['name']))
+    main(argv)
