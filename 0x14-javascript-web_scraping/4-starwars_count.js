080676211c850eac7500d097353711e3ae2ce114
Olaniyi2959 <olaniyiafolayan@yahoo.com>
2024-04-29 17:42:08 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
+// Number of films with the given character ID
 const request = require('request');
-const arg = process.argv[2];
+let num = 0;
 
-// use request to GET data from the API
-request(arg, (err, response, body) => {
-  if (err) {
-    console.error(err);
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    content.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(18)) {
+          num += 1;
+        }
+      });
+    });
+    console.log(num);
   }
-  // Store the parsed JSON data into an object
-  const obj = JSON.parse(body).results;
-  let count = 0;
-
-  // Using a forEach method, loop through the list to get all matching index
-  obj.forEach(objx => {
-    if (objx.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count++;
-    }
-  });
-  console.log(count);
 });
